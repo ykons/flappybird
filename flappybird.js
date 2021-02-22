ef293f1f9c4efaa23684a501ddba226e4a551d3f
Alexandro Silva <alexandro@ikons.com.br>
2021-02-21 21:45:18 -0300

    Refactor code that draws the player to a structured class

--- flappybird.js
+++ flappybird.js
@@ -4,9 +4,30 @@ const ctx = canvas.getContext("2d")
 const sprites = new Image()
 sprites.src = 'sprites.png'
 
-function gameLoop(timestamp) {
-  ctx.drawImage(sprites, 0, 0, 34, 25, 10, 200, 34, 25)
+let tickTime = 0
 
+const player = {
+  image: null,
+  spriteX: 0,
+  spriteY: 0,
+  width: 34,
+  height: 25,
+  positionX: 10,
+  positionY: 200,
+  update: (deltaTime) => {
+  },
+  render: () => {
+    ctx.drawImage(sprites, player.spriteX, player.spriteY, player.width, player.height, player.positionX, player.positionY, player.width, player.height)
+  }
+}
+
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - tickTime
+  tickTime = timestamp
+
+  player.update(deltaTime)
+  player.render()
+  
   window.requestAnimationFrame(gameLoop)
 }
 
