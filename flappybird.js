e51b107d8d7d679a4465b4dc79da2801174c353e
Alexandro Silva <alexandro@ikons.com.br>
2021-02-21 21:33:49 -0300

    Add game loop logic

--- flappybird.js
+++ flappybird.js
@@ -1,8 +1,13 @@
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("game")
+const ctx = canvas.getContext("2d")
 
-const sprites = new Image();
-sprites.onload = function () {
-  ctx.drawImage(sprites, 0, 0, 34, 25, 10, 200, 34, 25)
-}
+const sprites = new Image()
 sprites.src = 'sprites.png'
+
+function gameLoop(timestamp) {
+  ctx.drawImage(sprites, 0, 0, 34, 25, 10, 200, 34, 25)
+
+  window.requestAnimationFrame(gameLoop)
+}
+
+gameLoop(performance.now())
