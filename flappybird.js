811bc4019f11fa496986f6e0fe94f79368960b92
Alexandro Silva <alexandro@ikons.com.br>
2021-02-22 00:02:17 -0300

    Add background picture

--- flappybird.js
+++ flappybird.js
@@ -1,11 +1,27 @@
-const canvas = document.getElementById("game")
-const ctx = canvas.getContext("2d")
+const canvas = document.getElementById('game')
+const ctx = canvas.getContext('2d')
 
 const sprites = new Image()
 sprites.src = 'sprites.png'
 
 let tickTime = 0
 let gravity = 0.09
+
+const background = {
+  image: null,
+  spriteX: 390,
+  spriteY: 0,
+  width: 276,
+  height: 204,
+  positionX: 0,
+  positionY: canvas.height - 204,
+  update: (elapsedTime) => {
+  },
+  render: () => {
+    ctx.drawImage(sprites, background.spriteX, background.spriteY, background.width, background.height, background.positionX, background.positionY, background.width, background.height)
+    ctx.drawImage(sprites, background.spriteX, background.spriteY, background.width, background.height, background.positionX + background.width, background.positionY, background.width, background.height)
+  }
+}
 
 const player = {
   image: null,
@@ -31,10 +47,12 @@ function gameLoop(timestamp) {
   const elapsedTime = deltaTime / 1000
   tickTime = timestamp
   // clean canvas for next render
-  ctx.fillStyle = 'white';
+  ctx.fillStyle = `rgb(112, 197, 205)`;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+  background.update(elapsedTime)
   player.update(elapsedTime)
+  background.render()
   player.render()
   
   window.requestAnimationFrame(gameLoop)
