2468c92d1b99feacdc4254ed7a109014c5569eb0
Alexandro Silva <alexandro@ikons.com.br>
2021-02-26 20:18:32 -0300

    Adds game over condition and wraps sprite objects with a factory function

--- flappybird.js
+++ flappybird.js
@@ -31,81 +31,88 @@ function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const background = {
-  spriteX: 390,
-  spriteY: 0,
-  width: 276,
-  height: 204,
-  x: 0,
-  y: canvas.height - 204,
-  update: (deltaTime) => {},
-  render: () => {
-    ctx.fillStyle = `rgb(112, 197, 205)`; // sky color
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(
-      sprites,
-      background.spriteX,
-      background.spriteY,
-      background.width,
-      background.height,
-      background.x,
-      background.y,
-      background.width,
-      background.height
-    );
-    ctx.drawImage(
-      sprites,
-      background.spriteX,
-      background.spriteY,
-      background.width,
-      background.height,
-      background.x + background.width,
-      background.y,
-      background.width,
-      background.height
-    );
-  },
-};
+function createBackground() {
+  const background = {
+    spriteX: 390,
+    spriteY: 0,
+    width: 276,
+    height: 204,
+    x: 0,
+    y: canvas.height - 204,
+    update: (deltaTime) => {},
+    render: () => {
+      ctx.fillStyle = `rgb(112, 197, 205)`; // sky color
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(
+        sprites,
+        background.spriteX,
+        background.spriteY,
+        background.width,
+        background.height,
+        background.x,
+        background.y,
+        background.width,
+        background.height
+      );
+      ctx.drawImage(
+        sprites,
+        background.spriteX,
+        background.spriteY,
+        background.width,
+        background.height,
+        background.x + background.width,
+        background.y,
+        background.width,
+        background.height
+      );
+    },
+  };
+  return background;
+}
 
-const floor = {
-  spriteX: 0,
-  spriteY: 610,
-  width: 224,
-  height: 112,
-  x: 0,
-  y: canvas.height - 112,
-  velocityX: config.VELOCITY_FLOOR,
-  velocityY: 0,
-  carouselX: 0,
-  update: (deltaTime) => {
-    floor.carouselX += floor.velocityX * deltaTime;
-  },
-  render: () => {
-    x = floor.carouselX % config.CAROUSEL_LIMIT;
-    ctx.drawImage(
-      sprites,
-      floor.spriteX,
-      floor.spriteY,
-      floor.width,
-      floor.height,
-      x,
-      floor.y,
-      floor.width,
-      floor.height
-    );
-    ctx.drawImage(
-      sprites,
-      floor.spriteX,
-      floor.spriteY,
-      floor.width,
-      floor.height,
-      x + floor.width,
-      floor.y,
-      floor.width,
-      floor.height
-    );
-  },
-};
+function createFloor() {
+  const floor = {
+    spriteX: 0,
+    spriteY: 610,
+    width: 224,
+    height: 112,
+    x: 0,
+    y: canvas.height - 112,
+    velocityX: config.VELOCITY_FLOOR,
+    velocityY: 0,
+    carouselX: 0,
+    update: (deltaTime) => {
+      if (gameState.isGameOver()) return;
+      floor.carouselX += floor.velocityX * deltaTime;
+    },
+    render: () => {
+      x = floor.carouselX % config.CAROUSEL_LIMIT;
+      ctx.drawImage(
+        sprites,
+        floor.spriteX,
+        floor.spriteY,
+        floor.width,
+        floor.height,
+        x,
+        floor.y,
+        floor.width,
+        floor.height
+      );
+      ctx.drawImage(
+        sprites,
+        floor.spriteX,
+        floor.spriteY,
+        floor.width,
+        floor.height,
+        x + floor.width,
+        floor.y,
+        floor.width,
+        floor.height
+      );
+    },
+  };
+  return floor;
+}
 
 function createPairOfPipes(gapHeight) {
   const pairOfPipes = {
@@ -124,6 +131,7 @@ function createPairOfPipes(gapHeight) {
       return pairOfPipes.x + pairOfPipes.width < 0;
     },
     update: (deltaTime) => {
+      if (gameState.isGameOver()) return;
       pairOfPipes.x += pairOfPipes.velocityX * deltaTime;
     },
     render: () => {
@@ -154,100 +162,168 @@ function createPairOfPipes(gapHeight) {
   return Object.assign({}, pairOfPipes);
 }
 
-const player = {
-  spriteX: 0,
-  spriteY: 0,
-  width: 34,
-  height: 25,
-  x: 10,
-  y: 200,
-  velocityX: 0,
-  velocityY: 0,
-  jumpVelocity: config.VELOCITY_JUMP,
-  isJumping: false,
-  jump: () => {
-    player.isJumping = true;
-  },
-  update: (deltaTime) => {
-    if (player.isJumping) {
-      player.velocityY = -player.jumpVelocity;
-      player.isJumping = false;
-    }
-    player.velocityY += config.GRAVITY;
-    player.y += player.velocityY * deltaTime;
-
-    if (player.checkCollision([floor])) {
-      player.y = floor.y - player.height;
-      player.velocityY = 0;
-    }
-  },
-  checkCollision: (sprites) => {
-    let detected = false;
-    sprites.forEach((obj) => {
-      if (collisionDetection(player, obj)) {
-        detected = true;
+function createPlayer() {
+  const player = {
+    spriteX: 0,
+    spriteY: 0,
+    width: 34,
+    height: 25,
+    x: 10,
+    y: 200,
+    velocityX: 0,
+    velocityY: 0,
+    jumpVelocity: config.VELOCITY_JUMP,
+    isJumping: false,
+    jump: () => {
+      player.isJumping = true;
+    },
+    update: (deltaTime) => {
+      if (gameState.isGameOver()) return;
+      if (player.isJumping) {
+        player.velocityY = -player.jumpVelocity;
+        player.isJumping = false;
       }
-    });
-    return detected;
-  },
-  render: () => {
-    ctx.drawImage(
-      sprites,
-      player.spriteX,
-      player.spriteY,
-      player.width,
-      player.height,
-      player.x,
-      player.y,
-      player.width,
-      player.height
-    );
-  },
-};
+      player.velocityY += config.GRAVITY;
+      player.y += player.velocityY * deltaTime;
 
-const gameState = {
-  tickTime: 0,
-  frame: 0,
-  deltaTime: 0,
-  elapsedTime: 0,
-  layerBackground: [background],
-  layerObstacle: [],
-  layerForward: [floor, player],
-  tick: (timestamp) => {
-    const delta = timestamp - gameState.tickTime;
-    gameState.deltaTime = delta / 1000;
-    gameState.tickTime = timestamp;
-    gameState.elapsedTime += delta;
-    gameState.frame++;
-  },
-  update: () => {
-    gameState.removeOldObstacle();
-    gameState.createNewObstacle();
-  },
-  getSprites: () => {
-    return [
-      ...gameState.layerBackground,
-      ...gameState.layerObstacle,
-      ...gameState.layerForward,
-    ];
-  },
-  removeOldObstacle: () => {
-    gameState.layerObstacle = gameState.layerObstacle.filter((sprite) => {
-      return !sprite.isHidden();
-    });
-  },
-  createNewObstacle: () => {
-    if (gameState.frame % config.TIME_NEW_OBSTACLE == 0)
-      gameState.layerObstacle.push(
-        createPairOfPipes(
-          getRndInteger(config.OBSTACLE_RISE_MIN, config.OBSTACLE_RISE_MAX)
-        )
+      if (player.checkCollision([gameState.floor])) {
+        player.y = gameState.floor.y - player.height;
+        player.velocityY = 0;
+        gameState.gameOver();
+      }
+    },
+    checkCollision: (sprites) => {
+      let detected = false;
+      sprites.forEach((obj) => {
+        if (collisionDetection(player, obj)) {
+          detected = true;
+        }
+      });
+      return detected;
+    },
+    render: () => {
+      ctx.drawImage(
+        sprites,
+        player.spriteX,
+        player.spriteY,
+        player.width,
+        player.height,
+        player.x,
+        player.y,
+        player.width,
+        player.height
       );
-  },
-};
+    },
+  };
+  return player;
+}
+
+function createGameOver() {
+  const gameOver = {
+    spriteX: 134,
+    spriteY: 153,
+    width: 226,
+    height: 200,
+    x: canvas.width / 2 - 226 / 2,
+    y: 100,
+    update: (deltaTime) => {
+      if (!gameState.isGameOver()) return;
+    },
+    render: () => {
+      ctx.drawImage(
+        sprites,
+        gameOver.spriteX,
+        gameOver.spriteY,
+        gameOver.width,
+        gameOver.height,
+        gameOver.x,
+        gameOver.y,
+        gameOver.width,
+        gameOver.height
+      );
+    },
+  };
+  return gameOver;
+}
+
+function createGameState() {
+  const gameState = {
+    tickTime: 0,
+    frame: 0,
+    deltaTime: 0,
+    elapsedTime: 0,
+    state: "",
+    layerBackground: [],
+    layerObstacle: [],
+    layerForward: [],
+    layerGameOver: [],
+    player: {},
+    floor: {},
+    restart: () => {
+      gameState.player = createPlayer();
+      gameState.floor = createFloor();
+      gameState.tickTime = 0;
+      gameState.frame = 0;
+      gameState.deltaTime = 0;
+      gameState.elapsedTime = 0;
+      gameState.state = "playing";
+      gameState.layerBackground = [createBackground()];
+      gameState.layerObstacle = [];
+      gameState.layerForward = [gameState.floor, gameState.player];
+      gameState.layerGameOver = [createGameOver()];
+    },
+    tick: (timestamp) => {
+      const delta = timestamp - gameState.tickTime;
+      gameState.deltaTime = delta / 1000;
+      gameState.tickTime = timestamp;
+      gameState.elapsedTime += delta;
+      gameState.frame++;
+    },
+    update: () => {
+      if (!gameState.isGameOver()) {
+        gameState.removeOldObstacle();
+        gameState.createNewObstacle();
+      }
+    },
+    gameOver: () => {
+      gameState.state = "gameover";
+    },
+    isGameOver: () => {
+      return gameState.state === "gameover";
+    },
+    getSprites: () => {
+      let _sprites = [
+        ...gameState.layerBackground,
+        ...gameState.layerObstacle,
+        ...gameState.layerForward,
+      ];
+      if (gameState.isGameOver())
+        _sprites = [..._sprites, ...gameState.layerGameOver];
+      return _sprites;
+    },
+    removeOldObstacle: () => {
+      gameState.layerObstacle = gameState.layerObstacle.filter((sprite) => {
+        return !sprite.isHidden();
+      });
+    },
+    createNewObstacle: () => {
+      if (gameState.frame % config.TIME_NEW_OBSTACLE == 0)
+        gameState.layerObstacle.push(
+          createPairOfPipes(
+            getRndInteger(config.OBSTACLE_RISE_MIN, config.OBSTACLE_RISE_MAX)
+          )
+        );
+    },
+  };
+  return gameState;
+}
+
+const gameState = createGameState();
+gameState.restart();
 
 canvas.addEventListener("mousedown", (e) => {
-  player.jump();
+  if (gameState.state == "playing") gameState.player.jump();
+  if (gameState.state == "gameover") gameState.restart();
 });
 
 function gameLoop(timestamp) {
