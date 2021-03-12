5ec45d4cc888238999f200d85d5dfd41d6a2be6c
Alexandro Silva <alexandro@ikons.com.br>
2021-03-12 17:41:00 -0300

    Refactor pipes code

--- src/game/core/entities/sky-pipe.ts
+++ src/game/core/entities/sky-pipe.ts
@@ -7,27 +7,27 @@ import {
 } from "../../utils/const";
 import { Sprite } from "./sprite";
 
+const SPRITE_PIPE_HEIGHT = 400;
+
 export class SkyPipe extends Sprite {
-  constructor(floorPipeHeight: number) {
+  constructor(private readonly floorPipeHeight: number) {
     super();
     this.spriteX = 52;
-    this.spriteY =
-      169 +
-      400 -
-      (WORLD_HEIGHT -
-        config.FLOOR_HEIGHT -
-        floorPipeHeight -
-        config.PIPE_GAP_SPACE);
+    this.spriteY = 169 + SPRITE_PIPE_HEIGHT - this.getSkyPipeBottonY();
     this.width = 52;
-    this.height =
-      WORLD_HEIGHT -
-      config.FLOOR_HEIGHT -
-      floorPipeHeight -
-      config.PIPE_GAP_SPACE;
+    this.height = this.getFloorPipeTopY() - config.PIPES_GAP_SPACE;
     this.x = WORLD_WIDTH;
     this.y = 0;
     this.velocityX = config.VELOCITY_OBSTACLE;
     this.velocityY = 0;
+  }
+
+  getSkyPipeBottonY() {
+    return this.getFloorPipeTopY() - config.PIPES_GAP_SPACE;
+  }
+
+  getFloorPipeTopY() {
+    return WORLD_HEIGHT - config.FLOOR_HEIGHT - this.floorPipeHeight;
   }
 
   isHidden() {
